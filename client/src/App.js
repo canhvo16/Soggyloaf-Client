44559c3d5f33c76c1bf741c0bd244232aa84ebc1
canhvo16 <canhvo16@gmail.com>
2022-04-15 14:58:33 -0500

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-import Nav from './components/Nav';
+import Nav from './components/Nav'
 import React from 'react'
-import { useState } from 'react';
-import './style/App.css';
-import { Route, Routes } from 'react-router';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import About from './components/About';
+import { useState } from 'react'
+import './style/App.css'
+import { Route, Routes } from 'react-router'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import About from './components/About'
 import PlayList from './components/PlayList'
 import AnimeDetails from './components/AnimeDetails'
 
 function App() {
   const [user, setUser] = useState(null)
-
 
   return (
     <div className="App">
@@ -20,15 +19,15 @@ function App() {
         <Nav user={user} />
       </header>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login setUser={setUser} />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/playlist' element={<PlayList />} />
-        <Route path='/animedetails' element={<AnimeDetails />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/playlist" element={<PlayList />} />
+        <Route path="/animedetails" element={<AnimeDetails />} />
       </Routes>
     </div>
   )
 }
 
-export default App;
+export default App
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Register = () => {
   return (
-    <div className='register'>
-      <form className='register box'>
-        <input type="text" placeholder='Name' />
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='Password' />
-        <input type="password" placeholder='Confirm Password' />
+    <div className="register">
+      <form className="register box">
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
       </form>
     </div>
   )
