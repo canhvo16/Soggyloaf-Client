87804800e3200d53027f41c0e7454d6a52ebddfe
TakKwan <waslf3651@gmail.com>
2022-04-17 16:01:05 -0400

    setup anime details component

--- client/src/components/AnimeDetails.js
+++ client/src/components/AnimeDetails.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
+//anime details should get data from back end, not props
 const AnimeDetails = ({ id, title, popularity }) => {
+  const [animeDetails, setAnimeDetails] = useState({})
+
+
   return (
     <div>
       <h2>title: {title}</h2>
