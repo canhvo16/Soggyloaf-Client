8f4f8de64fcec34b1ebafccefeb2275a74b89742
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-04-19 17:43:35 -0400

    click scroll v1

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import AnimeCard from './AnimeCard'
+import { useRef } from 'react'
 import {
   getLongAnime,
   getNewAnime,
@@ -12,6 +13,11 @@ const Home = () => {
   const [romAnimes, setRomAnimes] = useState([])
   const [longAnimes, setLongAnimes] = useState([])
   const [newAnimes, setNewAnimes] = useState([])
+
+  let ref = useRef()
+  let ref1 = useRef()
+  let ref2 = useRef()
+  let ref3 = useRef()
 
   useEffect(() => {
     getTopAnimes(setTopAnimes)
@@ -29,47 +35,71 @@ const Home = () => {
     getNewAnime(setNewAnimes)
   })
 
+  const scroll = scrollOffset => {
+    ref.current.scrollLeft += scrollOffset
+  }
+
+  const scroll1 = scrollOffset => {
+    ref1.current.scrollLeft += scrollOffset
+  }
+
+  const scroll2 = scrollOffset => {
+    ref2.current.scrollLeft += scrollOffset
+  }
+
+  const scroll3 = scrollOffset => {
+    ref3.current.scrollLeft += scrollOffset
+  }
+
   return (
     <div className="scroll">
       <h1 className="carouselTitle"> Most Popular </h1>
       <section className="container">
-        <div className="wrapper">
+        <div className="wrapper" ref={ref}>
           {topAnimes.map(anime => (
             <AnimeCard key={anime.id} {...anime} />
           ))}
         </div>
+        <button onClick={() => scroll(-380)}>left</button>
+        <button onClick={() => scroll(380)}>right</button>
       </section>
 
       <div>
         <h1 className="carouselTitle"> Recently Added To SoggyLoaf </h1>
         <section className="container">
-          <div className="wrapper romance">
+          <div className="wrapper romance" ref={ref1}>
             {newAnimes.map(anime => (
               <AnimeCard key={anime.id} {...anime} />
             ))}
           </div>
+          <button onClick={() => scroll1(-380)}>left</button>
+          <button onClick={() => scroll1(380)}>right</button>
         </section>
       </div>
 
       <div>
         <h1 className="carouselTitle"> Romance </h1>
         <section className="container">
-          <div className="wrapper romance">
+          <div className="wrapper" ref={ref2}>
             {romAnimes.map(anime => (
               <AnimeCard key={anime.id} {...anime} />
             ))}
           </div>
+          <button onClick={() => scroll2(-380)}>left</button>
+          <button onClick={() => scroll2(380)}>right</button>
         </section>
       </div>
 
       <div>
         <h1 className="carouselTitle"> Got Some Time? </h1>
         <section className="container">
-          <div className="wrapper romance">
+          <div className="wrapper romance" ref={ref3}>
             {longAnimes.map(anime => (
               <AnimeCard key={anime.id} {...anime} />
             ))}
           </div>
+          <button onClick={() => scroll3(-380)}>left</button>
+          <button onClick={() => scroll3(380)}>right</button>
         </section>
       </div>
     </div>
